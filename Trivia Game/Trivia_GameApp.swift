75afdfaac14f4144f74026d4c7e90f42bb70a7e5
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-17 21:51:52 +0300

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Esma Koçak on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
