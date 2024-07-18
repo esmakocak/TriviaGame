8e165e53dd74a80bc94511fa3211bd753405c626
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-18 22:11:40 +0300

    Impemented modal "Trivia" for  trivia questions and answers

---
+++ Trivia Game/Modals/Trivia.swift
@@ -0,0 +1,45 @@
+//
+//  Trivia.swift
+//  Trivia Game
+//
+//  Created by Esma Koçak on 18.07.2024.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    var results: [Result]
+}
+
+struct Result: Decodable, Identifiable {
+    var id: UUID = UUID()
+    var category: String
+    var type: String
+    var difficulty: String
+    var question: String
+    var correctAnswer: String
+    var incorrectAnswers: [String]
+
+    var formattedQuestion: AttributedString {
+        do {
+            return try AttributedString(markdown: question)
+        } catch {
+            print("Error setting formattedQuestion: \(error)")
+            return ""
+        }
+    }
+
+    var answers: [Answer] {
+        do {
+            let correct = [Answer(text: try AttributedString(markdown: correctAnswer), isCorrect: true)]
+            let incorrects = try incorrectAnswers.map { answer in
+                Answer(text: try AttributedString(markdown: answer), isCorrect: false)
+            }
+            let allAnswers = correct + incorrects
+            return allAnswers.shuffled() // Randomize the order of answers
+        } catch {
+            print("Error setting answers: \(error)")
+            return []
+        }
+    }
+}
