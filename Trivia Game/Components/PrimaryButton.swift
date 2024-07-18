380ee8dcbdabd81095161bd4738d453d61f82bc9
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-18 14:33:11 +0300

    first commit

---
+++ Trivia Game/Components/PrimaryButton.swift
@@ -0,0 +1,26 @@
+//
+//  PrimaryButton.swift
+//  Trivia Game
+//
+//  Created by Esma Koçak on 18.07.2024.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: 10)
+    }
+}
+
+#Preview {
+    PrimaryButton(text: "Next")
+}
