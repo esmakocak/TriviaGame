d4be70ba07fa4392d437e2058268223d23484815
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-18 23:43:25 +0300

    Create TriviaManager class to fetch, decode, and manage trivia data

--- Trivia Game/TriviaManager.swift
+++ Trivia Game/TriviaManager.swift
@@ -6,13 +6,19 @@
 //
 
 import Foundation
-
-import Foundation
+import SwiftUI
 
 class TriviaManager: ObservableObject {
     private(set) var trivia: [Trivia.Result] = []
     @Published private(set) var length = 0
-
+    @Published private(set) var index = 0
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.00
+    @Published private(set) var score =  0
+    
     init() {
         Task.init {
             await fetchTrivia()
@@ -39,9 +45,39 @@ class TriviaManager: ObservableObject {
             DispatchQueue.main.async {
                 self.trivia = decodedData.results
                 self.length = self.trivia.count
+                self.setQuestion()
             }
         } catch {
             print("Error fetching trivia: \(error)")
+        }
+    }
+    
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            // Setting new question here....
+            setQuestion()
+        } else {
+            reachedEnd = true
+        }
+    }
+
+    func setQuestion(){
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length) * 350)
+        
+        if index < length {
+            let currentTriviaQuestion = trivia[index]
+            question = currentTriviaQuestion.formattedQuestion
+            answerChoices = currentTriviaQuestion.answers
+        }
+        
+    }
+    
+    func selectAnswer(answer: Answer){
+        answerSelected = true
+        if answer.isCorrect {
+            score += 1
         }
     }
 }
